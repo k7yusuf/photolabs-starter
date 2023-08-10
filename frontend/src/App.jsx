63313070e4b81f9174f34e6f2a001379941d10cd
k7yusuf <K7yusuf@gmail.com>
2023-08-10 17:01:52 -0400

    implement the 3 images for display, photolistitem

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,48 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
+const photos = [
+  {
+    id: "1",
+    location: {
+      city: "Montreal",
+      country: "Canada",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+    username: "Joe Example",
+    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+  },
+  {
+    id: "2",
+    location: {
+      city: "New York",
+      country: "USA",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
+    username: "Jane Doe",
+    profile: `${process.env.PUBLIC_URL}/profile-2.jpg`,
+  },
+  {
+    id: "3",
+    location: {
+      city: "Paris",
+      country: "France",
+    },
+    imageSource: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
+    username: "John Smith",
+    profile: `${process.env.PUBLIC_URL}/profile-3.jpg`,
+  },
+];
+
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <PhotoListItem/>
+      <div className="photo-list">
+        {photos.map(photo => (
+          <PhotoListItem key={photo.id} data={photo} />
+        ))}
+      </div>
     </div>
   );
 };
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,38 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+const PhotoListItem = (props) => {
+  const { imageSource, profile, username, location } = props.data;
+
+  return (
+    <div className="photo-item">
+      <img src={imageSource} alt="Photo" className="photo-image" />
+      <div className="photo-details">
+        <div className="user-profile">
+          <img src={profile} alt="Profile" className="profile-image" />
+          <span className="username">{username}</span>
+        </div>
+        <div className="location">
+          <span className="city">{location.city},</span>
+          <span className="country">{location.country}</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-const PhotoListItem = () => {
-  /* Insert React */
+PhotoListItem.propTypes = {
+  data: PropTypes.shape({
+    imageSource: PropTypes.string.isRequired,
+    profile: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    location: PropTypes.shape({
+      city: PropTypes.string.isRequired,
+      country: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
 };
 
 export default PhotoListItem;
